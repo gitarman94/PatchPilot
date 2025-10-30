1b1c7b25af90632c85de42dd0d874bd5df6487d4
gitarman94 <70583343+gitarman94@users.noreply.github.com>
2025-10-30 12:53:02 -0400

    Update setup_or_update_server.sh

--- setup_or_update_server.sh
+++ setup_or_update_server.sh
@@ -79,7 +79,7 @@ fi
 
 unzip -o latest.zip
 cd "${APP_DIR}"
-mv -f "/opt/patchpilot_install/PatchPilot-main/patchpilot_server" "${APP_DIR}"
+mv -f "/opt/patchpilot_install/PatchPilot-main/patchpilot_server/*" "${APP_DIR}"
 mv -f "/opt/patchpilot_install/PatchPilot-main/templates" "${APP_DIR}"
 mv -f "/opt/patchpilot_install/PatchPilot-main/server_test.sh" "${APP_DIR}"
 rm -rf "/opt/patchpilot_install"
