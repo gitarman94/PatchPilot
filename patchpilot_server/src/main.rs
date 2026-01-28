4d21a82fee8ded8f93c4e8b17678b4b793d310ef
gitarman94 <70583343+gitarman94@users.noreply.github.com>
2026-01-28 10:43:14 -0500

    Update

--- patchpilot_server/src/main.rs
+++ patchpilot_server/src/main.rs
@@ -18,7 +18,7 @@ use std::sync::{Arc, RwLock};
 use std::collections::HashMap;
 use db::{DbPool, initialize, get_conn};
 use state::{SystemState, AppState};
-use rocket::figment::providers::{Env, Toml};
+use rocket::figment::providers::{Env, Toml, Format};
 
 #[launch]
 fn rocket() -> _ {
